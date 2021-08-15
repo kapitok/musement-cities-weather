93669ff03f92b8c06f5597ff65944c7506c470d4
Mykhailo Lozynskyy <lozynskyy.m@gmail.com>
2021-08-15 21:23:09 +0200

    Added unit tests

--- tests/Unit/Infrastructure/Rest/WeatherApi/Response/WeatherResponseTest.php
+++ tests/Unit/Infrastructure/Rest/WeatherApi/Response/WeatherResponseTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Infrastructure\Rest\WeatherApi\Response;
 
+use Mlozynskyy\MusementWeather\Infrastructure\Rest\WeatherApi\Response\WeatherResponse;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -14,9 +15,49 @@ use PHPUnit\Framework\TestCase;
 class WeatherResponseTest extends TestCase
 {
 
-    public function testRepositoryIsWorkingProperly(): void
+    /**
+     * @dataProvider forecastResponseDataProvider
+     * @param array $forecastData
+     * @param array $expectedForecastData
+     */
+    public function testResponseObjectIsWorkingProperly(array $forecastData, array $expectedForecastData): void
     {
-        $this->markTestIncomplete();
+        $response = new WeatherResponse('New York', 52.37, 4.9, $forecastData);
+
+        $this->assertEquals(52.37, $response->getLatitude());
+        $this->assertEquals(4.9, $response->getLongitude());
+        $this->assertEquals($expectedForecastData, $response->getForecast());
+    }
+
+    /**
+     * @return array
+     */
+    // @codingStandardsIgnoreLine
+    public function forecastResponseDataProvider(): array
+    {
+        return [
+            [ [], [] ],
+            [
+                [ ['date' => '2021-08-14', 'day' => ['condition' => ['text' => 'Partly Cloudly']]] ],
+                [ ['date' => '2021-08-14', 'condition' => 'Partly Cloudly'] ],
+            ],
+            [
+                [ ['date' => '2021-08-14', 'day' => ['condition' => ['text' => null]]] ],
+                [ ['date' => '2021-08-14', 'condition' => ''] ],
+            ],
+            [
+                [ ['date' => '2021-08-14', 'day' => ['condition' => null]] ],
+                [ ['date' => '2021-08-14', 'condition' => ''] ],
+            ],
+            [
+                [ ['date' => '2021-08-14', 'day' => null] ],
+                [ ['date' => '2021-08-14', 'condition' => ''] ],
+            ],
+            [
+                [ ['date' => '2021-08-14'] ],
+                [ ['date' => '2021-08-14', 'condition' => ''] ],
+            ],
+        ];
     }
 
 }
